9aee5c3b926f25d4d2301021cd47f362ab70a597
anton.bashirov <anton.sh.local@gmail.com>
2023-11-06 01:31:54 +0200

    tests

--- test/dart/lib/call.dart
+++ test/dart/lib/call.dart
@@ -1,21 +1,21 @@
-import 'dart:ffi';
-import 'dart:io';
-
 import 'package:linux_interactor/interactor/defaults.dart';
 import 'package:linux_interactor/interactor/interactor.dart';
 import 'package:linux_interactor/interactor/worker.dart';
-import 'package:linux_interactor_test/bindings.dart';
-import 'package:path/path.dart';
+import 'package:linux_interactor_test/consumer.dart';
+import 'package:linux_interactor_test/producer.dart';
+import 'package:linux_interactor_test/test.dart';
 import 'package:test/test.dart';
 
 void testCall() {
   test("dart -> native", () async {
     final interactor = Interactor();
     final worker = InteractorWorker(interactor.worker(InteractorDefaults.worker()));
-    final bindings = TestBindings(DynamicLibrary.open("${dirname(Platform.script.toFilePath())}/../native/libinteractortest.so"));
+    final bindings = loadBindings();
     await worker.initialize();
-    worker.activate();
+    final producer = worker.producer(TestNativeProducer(bindings));
+    worker.consumer(TestNativeConsumer());
     bindings.test_interactor_initialize();
+    worker.activate();
     await interactor.shutdown();
   });
 }
--- test/dart/lib/test.dart
+++ test/dart/lib/test.dart
@@ -1,5 +1,12 @@
+import 'dart:ffi';
+
+import 'package:linux_interactor_test/bindings.dart';
 import 'package:linux_interactor_test/call.dart';
 import 'package:test/test.dart';
+import 'dart:io';
+import 'package:path/path.dart';
+
+TestBindings loadBindings() => TestBindings(DynamicLibrary.open("${dirname(Platform.script.toFilePath())}/../native/libinteractortest.so"));
 
 void main() {
   group("[call]", testCall);
