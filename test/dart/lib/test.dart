633c11fe73f81bd6c01506ae09a85aac86be6edf
anton.bashirov <anton.sh.local@gmail.com>
2023-11-13 00:06:21 +0200

    tests

--- test/dart/lib/test.dart
+++ test/dart/lib/test.dart
@@ -4,7 +4,6 @@ import 'dart:io';
 import 'package:linux_interactor_test/bindings.dart';
 import 'package:linux_interactor_test/call.dart';
 import 'package:linux_interactor_test/threading.dart';
-import 'package:linux_interactor_test/timeout.dart';
 import 'package:path/path.dart';
 import 'package:test/test.dart';
 
@@ -13,8 +12,8 @@ TestBindings loadBindings() => TestBindings(DynamicLibrary.open("${dirname(Platf
 void main() {
   group("[call native]", testCallNative);
   group("[call dart]", testCallDart);
+  group("[threading native]", testThreadingNative);
+  group("[threading dart]", testThreadingDart);
   // group("[timeout native]", testTimeoutNative);
   // group("[timeout dart]", testTimeoutDart);
-  // group("[threading native]", testThreadingNative);
-  // group("[threading dart]", testThreadingDart);
 }
--- test/dart/lib/threading.dart
+++ test/dart/lib/threading.dart
@@ -1,5 +1,7 @@
+import 'dart:async';
 import 'dart:ffi';
 
+import 'package:ffi/ffi.dart';
 import 'package:linux_interactor/interactor/defaults.dart';
 import 'package:linux_interactor/interactor/interactor.dart';
 import 'package:linux_interactor/interactor/worker.dart';
@@ -13,13 +15,18 @@ void testThreadingNative() {
     final interactor = Interactor();
     final worker = InteractorWorker(interactor.worker(InteractorDefaults.worker()));
     final bindings = loadBindings();
+    bindings.test_call_reset();
     await worker.initialize();
     final native = bindings.test_interactor_initialize();
     final producer = worker.producer(TestNativeProducer(bindings));
     worker.activate();
-    producer.testCallNativeEcho(native.ref.ring.ref.ring_fd);
-    bindings.test_call_native_check(native);
+    final call = producer.testCallNativeEcho(native.ref.ring.ref.ring_fd, configurator: (message) => message..setInputString("test"));
+    while (!bindings.test_call_native_check(native)) await Future.delayed(Duration(milliseconds: 100));
+    final result = await call;
+    expect(result.getOutputString(), "test");
+    result.release();
     await interactor.shutdown();
+    bindings.test_interactor_destroy(native);
   });
 }
 
@@ -28,11 +35,28 @@ void testThreadingDart() {
     final interactor = Interactor();
     final worker = InteractorWorker(interactor.worker(InteractorDefaults.worker()));
     final bindings = loadBindings();
+    bindings.test_call_reset();
     await worker.initialize();
     final native = bindings.test_interactor_initialize();
-    worker.consumer(TestNativeConsumer());
+    final completer = Completer();
+    worker.consumer(TestNativeConsumer(
+      (message) {
+        expect(message.getInputString(), "test");
+        completer.complete();
+      },
+    ));
     worker.activate();
-    bindings.test_call_native_check(native);
+    bindings.test_call_dart_string(native, worker.descriptor, 0, "test".toNativeUtf8().cast());
+    while (true) {
+      final result = bindings.test_call_dart_check(native);
+      if (result == nullptr) {
+        await Future.delayed(Duration(milliseconds: 100));
+        continue;
+      }
+      break;
+    }
+    await completer.future;
     await interactor.shutdown();
+    bindings.test_interactor_destroy(native);
   });
 }
