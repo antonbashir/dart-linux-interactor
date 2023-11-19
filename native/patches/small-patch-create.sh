3417d13b8904794eb398d839f34b5f322cf681fe
anton.bashirov <anton.sh.local@gmail.com>
2023-11-19 17:22:36 +0200

    fixes

--- native/patches/small-patch-create.sh
+++ native/patches/small-patch-create.sh
@@ -1,3 +1,6 @@
 #!/bin/bash
 
-diff --unified --recursive --no-dereference patches/small-old/ patches/small-new/ > patches/small-patch.diff
+cd patches/
+diff --unified --recursive --no-dereference patches/small-old/ patches/small-new/ >> patches/small-patch.diff
+rm -rf patches/small-old
+rm -rf patches/small-new
---
+++ native/patches/small-patch-prepare.sh
@@ -0,0 +1,6 @@
+#!/bin/bash
+
+rm -rf patches/small-old
+rm -rf patches/small-new
+cp -r  small patches/small-old
+cp -r  small patches/small-new
