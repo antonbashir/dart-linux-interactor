f677fe5dfe3b8326e09a76d2893f1cd96a320ac8
anton.bashirov <anton.sh.local@gmail.com>
2023-11-25 21:30:50 +0200

    fixes

--- dart/lib/linux_interactor.dart
+++ dart/lib/linux_interactor.dart
@@ -2,7 +2,7 @@ export 'interactor/buffers.dart' show InteractorBuffers;
 export 'interactor/calls.dart' show InteractorCall;
 export 'interactor/configuration.dart' show InteractorConfiguration;
 export 'interactor/data.dart' show InteractorDatas;
-export 'interactor/declaration.dart' show InteractorCallback, InteractorConsumer, InteractorMethod, InteractorProducer;
+export 'interactor/declaration.dart' show InteractorCallback, InteractorConsumer, InteractorMethod, InteractorProducer, InteractorProducerRegistrat;
 export 'interactor/defaults.dart' show InteractorDefaults;
 export 'interactor/notifications.dart' show InteractorNotification;
 export 'interactor/payloads.dart' show InteractorPayloads;
